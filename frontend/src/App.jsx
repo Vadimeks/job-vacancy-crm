fbcd0e6f4a3d5faea6dfb29f6e89a38b32c083c1
Vadzim <eks220885@gmail.com>
2026-07-23 14:00:41 +0300

    refactor: update bot links to new public survey URL and mask CRM routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,21 +12,29 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         {/* 👈 Анкета БЕЗ лэйаўта (чысты экран для Тэлеграма) */}
-        <Route path="/survey" element={<Survey />} />
+        <Route path="/anketa" element={<Survey />} />
+{/* Дадаем рэдырэкт са старога адраса, каб не зламаць бот, пакуль не абнавім яго */}
+<Route path="/survey" element={<Survey />} />
 
-        {/* Усе астатнія старонкі ЎНУТРЫ лэйаўта */}
-        <Route path="*" element={
-          <Layout>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/vacancies" element={<Vacancies />} />
-              <Route path="/candidates" element={<Candidates />} />
-              <Route path="/candidates/:id" element={<Candidates />} />
-              <Route path="/templates" element={<Templates />} />
-              <Route path="/inbox" element={<Inbox />} />
-            </Routes>
-          </Layout>
-        } />
+       {/* 🔐 Схаваная CRM (Адмін-панэль) */}
+<Route path="/nova-management-secure-2024/*" element={
+  <Layout>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/vacancies" element={<Vacancies />} />
+      <Route path="/candidates" element={<Candidates />} />
+      <Route path="/candidates/:id" element={<Candidates />} />
+      <Route path="/templates" element={<Templates />} />
+      <Route path="/inbox" element={<Inbox />} />
+    </Routes>
+  </Layout>
+} />
+
+{/* 📄 Будучая старонка CV (заглушка) */}
+<Route path="/cv" element={<div className="p-10 text-center font-sans">Генератор CV у розробці...</div>} />
+
+{/* 🏠 Галоўная старонка (калі хтосьці зойдзе проста на дамен) */}
+<Route path="/" element={<div className="p-10 text-center font-sans">Nova Work Agency - Офіційний сайт</div>} />
       </Routes>
     </BrowserRouter>
   );
